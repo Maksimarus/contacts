4566c7fde3e858948c6ced34206fb78998dcab93
Maksim Chuchuk <cucukmaksim@gmail.com>
2022-08-19 19:41:38 +0300

    feat: опубликовать на gh-pages

--- src/components/UI/MyInput.tsx
+++ src/components/UI/MyInput.tsx
@@ -2,11 +2,13 @@ import React, { InputHTMLAttributes } from "react";
 
 interface MyInputProps extends InputHTMLAttributes<HTMLInputElement> {
   addClassName?: string;
-  ref?: React.Ref<any>;
 }
 
 const MyInput = React.forwardRef(
-  ({ addClassName, ...props }: MyInputProps, ref) => {
+  (
+    { addClassName, ...props }: MyInputProps,
+    ref: React.Ref<HTMLInputElement>
+  ) => {
     return (
       <input
         className={`w-full p-3 bg-gray-800 rounded-xl ${addClassName}`}
